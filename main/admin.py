67f0b1cfaf8a268a2258546e9c60be7eb0260304
AbazMax <maxim.abazov@gmail.com>
2022-10-26 13:59:33 +0300

    some models added

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category, Promo, Product
+from .models import Category, Promo, Product, Testimonials, About, WhyUs, Partners, Banner, Info, Contacts
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -20,3 +20,34 @@ class PromoAdmin(admin.ModelAdmin):
     list_filter = ('type', 'position',)
     list_display = ('promo_product', 'type', 'end_of_promo', 'is_visible',)
 
+
+@admin.register(Testimonials)
+class TestimonialsAdmin(admin.ModelAdmin):
+    list_display = ('position', 'name', 'is_visible')
+
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ('title_line_1', 'title_line_2')
+
+
+@admin.register(WhyUs)
+class WhyUsAdmin(admin.ModelAdmin):
+    list_display = ('delivery_title', 'price_title', 'service_title', 'refund_title')
+
+
+@admin.register(Partners)
+class PartnersAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_visible')
+
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_visible')
+
+@admin.register(Info)
+class InfoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    pass
--- main/models.py
+++ main/models.py
@@ -75,3 +75,144 @@ class Promo(models.Model):
         verbose_name_plural = ('Promo')
 
 
+class Testimonials(models.Model):
+
+    def get_file_name(self, filename: str) -> str:
+        ext_file = filename.strip().split('.')[-1]
+        new_filename = f'{uuid.uuid4()}.{ext_file}'
+        return os.path.join('Testimonials', new_filename)
+
+    name = models.CharField(max_length=100, unique=True)
+    profession = models.CharField(max_length=50)
+    review = models.TextField(max_length=300)
+    position = models.SmallIntegerField()
+    photo = models.ImageField(upload_to=get_file_name)
+    is_visible = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('position',)
+        verbose_name_plural = 'Testimonials'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class About(models.Model):
+
+    def get_file_name(self, filename: str) -> str:
+        ext_file = filename.strip().split('.')[-1]
+        new_filename = f'{uuid.uuid4()}.{ext_file}'
+        return os.path.join('About', new_filename)
+
+    title_line_1 = models.CharField(max_length=50, blank=True, db_index=True)
+    title_line_2 = models.CharField(max_length=50, blank=True, db_index=True)
+    paragraph_1 = models.TextField(max_length=500, blank=True, db_index=True)
+    paragraph_2 = models.TextField(max_length=500, blank=True, db_index=True)
+    video_link = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to=get_file_name)
+
+    class Meta:
+        verbose_name_plural = 'About section'
+
+    def __str__(self):
+        return f'About'
+
+
+class WhyUs(models.Model):
+
+    def get_file_name(self, filename: str) -> str:
+        ext_file = filename.strip().split('.')[-1]
+        new_filename = f'{uuid.uuid4()}.{ext_file}'
+        return os.path.join('WhyUs', new_filename)
+
+    delivery_title = models.CharField(max_length=50)
+    delivery_text = models.TextField(max_length=200)
+    price_title = models.CharField(max_length=50)
+    price_text = models.TextField(max_length=200)
+    service_title = models.CharField(max_length=50)
+    service_text = models.TextField(max_length=200)
+    refund_title = models.CharField(max_length=50)
+    refund_text = models.TextField(max_length=200)
+    photo = models.ImageField(upload_to=get_file_name)
+
+    class Meta:
+        verbose_name_plural = 'WhyUs'
+
+    def __str__(self):
+        return f'WhyUs'
+
+
+class Partners(models.Model):
+
+    def get_file_name(self, filename: str) -> str:
+        ext_file = filename.strip().split('.')[-1]
+        new_filename = f'{uuid.uuid4()}.{ext_file}'
+        return os.path.join('partners_logos', new_filename)
+
+    name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to=get_file_name, )
+    is_visible = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = 'Partners logos'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Banner(models.Model):
+
+    def get_file_name(self, filename: str) -> str:
+        ext_file = filename.strip().split('.')[-1]
+        new_filename = f'{uuid.uuid4()}.{ext_file}'
+        return os.path.join('Banner', new_filename)
+
+    name = models.CharField(max_length=50)
+    banner_image = models.ImageField(upload_to=get_file_name, help_text='Recommended resolution: 1920x494px' )
+    is_visible = models.BooleanField(default=True, )
+
+    class Meta:
+        verbose_name_plural = 'Banner'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Info(models.Model):
+
+    def get_file_name(self, filename: str) -> str:
+        ext_file = filename.strip().split('.')[-1]
+        new_filename = f'{uuid.uuid4()}.{ext_file}'
+        return os.path.join('logo', new_filename)
+
+    brand_name = models.CharField(max_length=50, db_index=True)
+    brand_logo = models.ImageField(upload_to=get_file_name, )
+    main_title_line_1 = models.CharField(max_length=200, db_index=True)
+    main_title_line_2 = models.CharField(max_length=200, db_index=True)
+    about_title_line_1 = models.CharField(max_length=200, db_index=True)
+    about_title_line_2 = models.CharField(max_length=200, db_index=True)
+    contact_title_line_1 = models.CharField(max_length=200, db_index=True)
+    contact_title_line_2 = models.CharField(max_length=200, db_index=True)
+    shop_title_line_1 = models.CharField(max_length=200, db_index=True)
+    shop_title_line_2 = models.CharField(max_length=200, db_index=True)
+    our_team_title = models.TextField(max_length=200)
+    related_prod_title = models.TextField(max_length=200)
+
+
+    class Meta:
+        verbose_name_plural = 'Main information'
+
+    def __str__(self):
+        return f'Main information'
+
+
+class Contacts(models.Model):
+    text = models.TextField(max_length=500)
+    address = models.TextField(max_length=500)
+    open_hours = models.TextField(max_length=1000)
+    contact = models.TextField(max_length=500)
+    about_short = models.TextField(max_length=300)
+    subscribe_text = models.TextField(max_length=200)
+    facebook = models.CharField(max_length=200)
+    instagram = models.CharField(max_length=200)
+    linkedin = models.CharField(max_length=200)
